73a827032636e3e5863dfbcfa9c27df699eb616a
Michael Davidson <michaeldavidson@ccs.neu.edu>
2017-04-04 19:08:59 -0400

    Basic DNS Server

--- dnsserver.py
+++ dnsserver.py
@@ -3,8 +3,12 @@ import socket
 import struct
 import argparse
 from random import randint
+import thread
 
-EC2_HOSTS = {'ec2-54-166-234-74.compute-1.amazonaws.com':'54.166.234.74',
+class CDNLogic:
+
+    def __init__(self):
+        self.EC2_HOSTS = {'ec2-54-166-234-74.compute-1.amazonaws.com':'54.166.234.74',
              'ec2-52-90-80-45.compute-1.amazonaws.com':'52.90.88.45',
              'ec2-54-183-23-203.us-west-1.compute.amazonaws.com':'54.183.23.203',
              'ec2-54-70-111-57.us-west-2.compute.amazonaws.com':'54.70.111.57',
@@ -14,6 +18,10 @@ EC2_HOSTS = {'ec2-54-166-234-74.compute-1.amazonaws.com':'54.166.234.74',
              'ec2-52-62-198-57.ap-southeast-2.compute.amazonaws.com':'52.62.198.57',
              'ec2-52-192-64-163.ap-northeast-1.compute.amazonaws.com':'52.192.64.163',
              'ec2-54-233-152-60.sa-east-1.compute.amazonaws.com':'54.233.152.60'}
+
+    def find_best_replica(self, client_addr):
+        return '54.233.152.60'
+
 
 class Packet:
     """
@@ -141,9 +149,50 @@ class Packet:
 
 
 class DNSServer:
-    def __init__(self):
+    def __init__(self, port, name):
+        self.cdn_logic = CDNLogic()
+        self.client_locations = {}
+        self.sock = -1
+        self.port = port
+        self.name = name
+        self.my_ip = self.get_ipaddr()
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.sock.bind((self.my_ip, self.port))
+        except:
+            sys.exit("Failed to create socket.")
 
+    def get_ipaddr(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('cs5700cdnproject.ccs.neu.edu', 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
 
+    def run_server(self):
+        while True:
+            try:
+                request, client = self.sock.recvfrom(65535)
+                thread.start_new_thread(self.handle_request, request, client)
+            except:
+                sys.exit("Error receiving data or creating thread.")
+        #self.sock.close()
+
+    def handle_request(self, request, client):
+        packet = Packet()
+        packet.parse_question(request)
+
+        if client[0] in self.client_locations:
+            best_server = self.client_locations[client[0]]
+        else:
+            best_server = self.cdn_logic.find_best_replica(client[0])
+
+        dns_response = packet.generate_answer(self.name, best_server)
+
+        try:
+            self.sock.sendto(dns_response, best_server)
+        except:
+            sys.exit("Failed to send DNS Answer.")
 
 
 if __name__== '__main__':
@@ -151,6 +200,7 @@ if __name__== '__main__':
     parser.add_argument('-p',dest='port',type=int)
     parser.add_argument('-n',dest='name')
     args = parser.parse_args()
-    server = DNSServer()
+    server = DNSServer(args.port, args.name)
+    server.run_server()
 
 
