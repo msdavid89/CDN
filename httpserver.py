cbc662d287476a9333a2ed9faa18ee2b8e4510f2
Michael Davidson <michaeldavidson@ccs.neu.edu>
2017-04-14 21:48:46 -0400

    Added caching algorithm, bugs still exist.

--- httpserver.py
+++ httpserver.py
@@ -3,6 +3,7 @@ import sys
 import socket
 import argparse
 import thread
+from threading import RLock
 import os
 import requests
 import json
@@ -16,12 +17,15 @@ class CacheHandler:
         self.dns_port = dns_port
         self.cache = {}  # Dictionary of file path: file size
         self.popularity = {} # Dictionary of file path: popularity as # of hits from CSV
+        self.constraints = {} # Dictionary of file path: (size, popularity) for cached pages
         self.available_space = MAX_CACHE
         self.cache_directory = os.getcwd() + '/wiki_cache'
-        self.cache_lock = thread.allocate_lock()
+        self.cache_lock = RLock()
         self.space_lock = thread.allocate_lock()
+        self.constraints_lock = thread.allocate_lock()
         self.load_local_cache(self.cache_directory)
         self.load_popularity_from_csv()
+        self.update_constraints()
         try:
             # Create socket for DNS Server connection, used for active measurements
             # and/or passing cache info to DNS server.
@@ -65,16 +69,18 @@ class CacheHandler:
         except:
             print("Failed to load popularity dictionary from CSV.")
 
-
+    def reencode(self, f):
+        for line in f:
+            yield line.decode('windows-1252').encode('utf-8')
 
     def load_local_cache(self, path):
         """This is used to restore the cache dictionary when the server is restarted."""
         if not os.path.exists(path):
             os.mkdir(path)
+            os.mkdir(path + '/wiki')
             return
         for f in os.listdir(path):
             file_name = path + '/' + f
-            print("file_name: " + file_name)
             if os.path.isdir(file_name):
                 self.load_local_cache(file_name)
             elif os.path.isfile(file_name):
@@ -102,22 +108,103 @@ class CacheHandler:
         if size <= self.available_space:
             self.space_lock.release()
             self.add_to_cache(self.cache_directory + path, contents)
-        if size > self.available_space:
+        else:
             self.space_lock.release()
-            # TODO: Implement caching strategy
+            self.knapsack(path, contents, size)
+
+        self.update_constraints()
 
         # Tell DNS Server about new cache updates.
         self.handle_dns()
 
+    def update_constraints(self):
+        """Update the constraints dictionary to reflect size:popularity of currently cached
+            content for use in caching algorithm."""
+        self.constraints_lock.acquire()
+        self.cache_lock.acquire()
+        for path in self.cache.keys():
+            pos = path.rfind('/') + 1 # Find the position where the file name begins
+            page = path[pos:]
+            try:
+                self.constraints[path] = (self.cache[path], self.popularity[page])
+            except:
+                print("Error thrown for path: " + path)
+                path = unicode(path)
+                print("Unicode attempt: " + path)
+                path.encode('utf-8')
+                print("UTF-8: " + path)
+        self.cache_lock.release()
+        self.constraints_lock.release()
+
+    def knapsack(self, path, contents, size):
+        """Find the lowest cumulative popularity of files in cache which can be removed in
+            order to make room for the potential new cache file. Decide if it is worthwhile
+            to remove those files.
+            
+            Note: This is a solution to the knapsack problem
+        """
+
+        # Add new path using constraints dictionary so that cache_items provides a full list
+        # for cache optimization subject to knapsack problem.
+        pos = path.rfind('/') + 1
+        page = path[pos:]
+        self.constraints_lock.acquire()
+        cache_items = self.constraints.items()
+        self.constraints_lock.release()
+        cache_items.append((path, (size, self.popularity[page])))
+        cache_items = tuple(cache_items)
+
+        new_cache = {}
+        solution = self.solve_knapsack(cache_items, new_cache, MAX_CACHE)
+        solution = dict(solution)
+        #print("----solution----\n" + str(solution))
+
+        self.cache_lock.acquire()
+        try:
+            for x in self.cache.keys():
+                if x not in solution:
+                    # Remove any cached items that are suboptimal
+                    #print("Not in solution: " + x)
+                    self.remove_from_cache(x)
+            if path in solution:
+                # Add new page to cache if optimal
+                #print("Adding to cache: " + path)
+                self.add_to_cache(self.cache_directory + path, contents)
+        finally:
+            self.cache_lock.release()
+
+    def solve_knapsack(self, cache_items, new_cache, maximum):
+        """This is the recursive knapsack solving algorithm"""
+        if not cache_items:
+            return ()
+        if (cache_items, maximum) not in new_cache:
+            head = cache_items[0]
+            tail = cache_items[1:]
+            include = (head,) + self.solve_knapsack(tail, new_cache, maximum - head[1][0])
+            dont_include = self.solve_knapsack(tail, new_cache, maximum)
+            if self.total_popularity(include) > self.total_popularity(dont_include):
+                answer = include
+            else:
+                answer = dont_include
+            new_cache[(cache_items, maximum)] = answer
+        return new_cache[(cache_items, maximum)]
+
+    def total_popularity(self, values):
+        """Helper function for knapsack solution. Returns total popularity of a possible
+            cache."""
+        total_size = 0
+        total_pop = 0
+        for v in values:
+            total_size += v[1][0]
+            total_pop += v[1][1]
+        return total_pop if total_size <= MAX_CACHE else 0
 
     def check_cache(self, path):
         """Checks if a given path is already saved in our cache. If so, return true."""
         path = self.cache_directory + path
         self.cache_lock.acquire()
-        print("Path: " + path)
-        print("Cache: " + str(self.cache))
         if path in self.cache:
-            print("Match!")
+            print("\nMATCH!!! on path: %s\n", path)
             self.cache_lock.release()
             return True
         self.cache_lock.release()
@@ -127,27 +214,27 @@ class CacheHandler:
         """Saves a new file to the cache, returning True if successful. Saves the contents
             of a given file to the location specified by path."""
         try:
-            file = open(path, 'w')
-            file.write(contents)
-            file.close()
+            f = open(path, 'w')
+            f.write(contents)
+            f.close()
             try:
                 # Update cache and cache size to reflect new file
                 size = os.path.getsize(path)
-                print("Size: " + str(size))
                 self.cache_lock.acquire()
                 self.cache[path] = size
-                print("Cache: " + str(self.cache))
                 self.cache_lock.release()
                 self.space_lock.acquire()
                 self.available_space -= size
-                print("Space available: " + str(self.available_space))
+                #print("\n\nSpace available: " + str(self.available_space))
                 self.space_lock.release()
             except:
                 self.cache_lock.release()
                 self.space_lock.release()
                 return False
         except:
+            print("Failed to write file %s to disk" % path)
             return False
+
         return True
 
     def read_from_cache(self, path):
@@ -164,7 +251,6 @@ class CacheHandler:
 
     def remove_from_cache(self, path):
         """Given a file path, remove that file from the disk cache and update space available."""
-        path = self.cache_directory + path
         try:
             os.remove(path)
             self.cache_lock.acquire()
@@ -193,7 +279,7 @@ class HTTPServer:
             self.serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.serv_sock.bind(('', self.port))
-            self.serv_sock.listen(10)
+            self.serv_sock.listen(30)
         except:
             sys.exit("Failed to create socket.")
 
@@ -206,7 +292,13 @@ class HTTPServer:
                 cli_sock, addr = self.serv_sock.accept()
                 thread.start_new_thread(self.connection_handler, (cli_sock, addr))
             except:
-                sys.exit("Error accepting connection or spawning thread for it.")
+                if self.cache.space_lock.locked():
+                    self.cache.space_lock.release()
+                if self.cache.constraints_lock.locked():
+                    self.cache.constraints_lock.release()
+                thread.interrupt_main()
+                sys.exit("Error accepting connection.")
+
 
     def connection_handler(self, sock, addr):
         """Manage the connection with a given client."""
@@ -229,13 +321,10 @@ class HTTPServer:
         if self.cache.check_cache(path):
             # Get content from cache, construct HTTP response and send it to the client.
             content = self.cache.read_from_cache(path)
-            print("Read from cache.")
             length = os.path.getsize(self.cache.cache_directory + path)
-            print("Got cache size.")
             response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: '
             response += str(length) + '\r\n\r\n' + content
             try:
-                print("Cache hit!")
                 sock.sendall(response)
             except:
                 sys.exit("Failed to send cached response to client.")
@@ -261,7 +350,8 @@ class HTTPServer:
                 sock.sendall(response)
                 self.cache.update_cache(path, r.content)
             else:
-                response = 'HTTP/1.1 404 Not Found'
+                print("\n\n-----Not 200 OK------")
+                response = 'HTTP/1.1 404 Not Found\r\n'
                 sock.sendall(response)
 
         sock.close()
---
+++ test_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import csv
+import subprocess
+from random import randint
+import os
+import thread
+
+directory = os.getcwd()
+popularity = {}
+try:
+    csv_file = open('cdn_popularity.csv', 'r')
+except:
+    print("Failed to open csv popularity file (cdn_popularity.csv).")
+try:
+    reader = csv.reader(csv_file)
+    csv_list = list(reader)
+    for row in reader:
+        popularity[row[0]] = int(row[1])
+    csv_file.close()
+except:
+    print("Failed to load popularity dictionary from CSV.")
+
+
+def wget_func(q, p, d):
+    try:
+        subprocess.check_output(["wget", q])
+        to_remove = directory + '/' + p
+        print(to_remove)
+        os.remove(to_remove)
+    except:
+        print("\n\nQuery: " + q + " returned error.\n\n")
+
+
+count = 0
+while count < 20:
+    count += 1
+    r = randint(0,4999)
+    path = csv_list[r][0]
+    query = "http://ec2-52-90-80-45.compute-1.amazonaws.com:40009/wiki/%s" % (path)
+    wget_func(query, path, directory)
