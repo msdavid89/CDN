183525bdf493714308c419a278e513c51a942174
akinade <akinade.o@husky.neu.edu>
2017-04-17 16:35:22 -0700

    reformating

--- runCDN.py
+++ runCDN.py
@@ -14,31 +14,31 @@ replicas_servers = [
 "ec2-54-233-152-60.sa-east-1.compute.amazonaws.com"]
 
 def validate_args():
-    port_no = ''
+    port = ''
     origin_server = ''
     cdn_server = 'cs5700cdnproject.ccs.neu.edu'
     RSA_key_path = ''
     username = ''
-    resolve_name = ''
+    name = ''
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "p:o:n:u:i:h" ,["help"])
     except getopt.GetoptError:
-        print "./[deploy|run|stop]CDN -p <port_no> -o <origin> -n <name> -u <username> -i <keyfile>"
+        print "./[deploy|run|stop]CDN -p <port> -o <origin> -n <name> -u <username> -i <keyfile>"
         sys.exit(2)
 
     for opt, arg in opts:
         if opt in ('-h', '--help'):
-            print "./[deploy|run|stop]CDN -p <port_no> -o <origin> -n <name> -u <username> -i <keyfile>"
+            print "./[deploy|run|stop]CDN -p <port> -o <origin> -n <name> -u <username> -i <keyfile>"
             sys.exit(1)
         elif opt in ('-p'):
-            port_no = arg
+            port = arg
 
         elif opt in ('-o'):
             origin_server = arg
 
         elif opt in ('-n'):
-            resolve_name = arg
+            name = arg
 
         elif opt in ('-u'):
             username = arg
@@ -47,26 +47,27 @@ def validate_args():
             RSA_key_path = arg
 
 #recieve value from the command line and return.
-    return port_no, origin_server, cdn_server, username, RSA_key_path, resolve_name
+    return port, origin_server, cdn_server, username, RSA_key_path, name
 
 #function to run sripts (dnsserver) remotely  
-def run_dns_server(port_no, cdn_server, username, RSA_key_path, resolve_name):
-    run_dns = "ssh -i "+ RSA_key_path +" "+ username + "@" + cdn_server + " nohup" + " python " + "dnsserver.py -p "+ port_no + " -n "+ resolve_name + " &"
+def run_dns_server(port, cdn_server, username, RSA_key_path, name):
+    run_dns = "ssh -i "+ RSA_key_path +" "+ username + "@" + cdn_server + " nohup" + " python " + "dnsserver.py -p "+ port + " -n "+ name + " &"
     print run_dns
     subprocess.check_output(run_dns, shell=True)
 
 
 #    print "dnsserver running...."
 #function to run httpserver on the replicas(from the lists above).
-def run_http_servers(port_no, origin_server, username, RSA_key_path, resolve_name):
+
+def run_http_servers(port, origin_server, username, RSA_key_path,):
     for replicas in replicas_servers:
-        run_http = "ssh -i "+ RSA_key_path +" "+ username + "@" + cdn_server + " nohup" + " python " + "dnsserver.py -p "+ port_no + " -o "+ origin_server + " &"
-#	print run_http
+        run_http = "ssh -i "+ RSA_key_path +" "+ username + "@" + replicas + " nohup" + " python " + "httpserver.py -p "+ port + " -o "+ origin_server + " &"
+	print run_http
         subprocess.check_output(run_http ,shell=True)
 #	print "httpserver running...."
 
 if __name__ == '__main__':
 
-    port_no, origin_server, cdn_server, username, RSA_key_path, resolve_name = validate_args()
-    run_dns_server(port_no, cdn_server, username, RSA_key_path, resolve_name)
-    run_http_servers(port_no, origin_server, username, RSA_key_path, resolve_name)
+    port, origin_server, cdn_server, username, RSA_key_path, name = validate_args()
+    run_dns_server(port, cdn_server, username, RSA_key_path, name)
+    run_http_servers(port, origin_server, username, RSA_key_path )
